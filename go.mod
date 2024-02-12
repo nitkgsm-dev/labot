2091b4411e891b7ed80cbbf6af1f78d093400671
ikafly144 <support@sabafly.net>
2024-02-12 21:42:33 +0900

    feat: add main.go and ping command

--- go.mod
+++ go.mod
@@ -1,4 +1,20 @@
 module github.com/nitkgsm-dev/labot
 
-go 1.21.6
 go 1.22
+
+require (
+	github.com/chzyer/readline v1.5.1
+	github.com/disgoorg/disgo v0.17.1
+)
+
+require (
+	github.com/disgoorg/json v1.1.0 // indirect
+	github.com/disgoorg/snowflake/v2 v2.0.1 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/sasha-s/go-csync v0.0.0-20240107134140-fcbab37b09ad // indirect
+	golang.org/x/crypto v0.19.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+)
+
+replace github.com/disgoorg/disgo v0.17.1 => github.com/nitkgsm-dev/disgo v0.0.0-20240212123036-7c2a59751862
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"github.com/chzyer/readline"
+	"github.com/disgoorg/disgo"
+	"github.com/disgoorg/disgo/bot"
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/events"
+	"github.com/disgoorg/disgo/handler"
+	"github.com/disgoorg/disgo/handler/middleware"
+	"log/slog"
+	"os"
+)
+
+var (
+	token = os.Getenv("discord_token")
+)
+
+func main() {
+	l, err := readline.NewEx(&readline.Config{
+		Prompt:          "> ",
+		HistoryFile:     ".labot/.console_history",
+		AutoComplete:    completer,
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		panic(err)
+	}
+	defer l.Close()
+	l.CaptureExitSignal()
+
+	slog.SetDefault(slog.New(slog.NewTextHandler(l.Stderr(), nil)))
+
+	mux := handler.New()
+	mux.Use(middleware.Logger)
+
+	mux.Command("/ping", func(e *handler.CommandEvent) error {
+		messenger, err := e.Client().WebhookManager().GetMessenger(e.Channel())
+		if err != nil {
+			return err
+		}
+		if _, err := messenger.Send(discord.NewMessageBuilder().SetContent("pong"), e.Client()); err != nil {
+			return err
+		}
+		return e.CreateMessage(discord.NewMessageBuilder().SetContent("pong").BuildCreate())
+	})
+
+	mux.NotFound(func(e *events.InteractionCreate) error {
+		slog.Warn("not found", slog.Any("interaction", e.Interaction))
+		return nil
+	})
+
+	client, err := disgo.New(token, bot.WithDefaultGateway())
+	if err != nil {
+		panic(err)
+	}
+
+	client.AddEventListeners(mux)
+
+	if err := handler.SyncCommands(client, commands, nil); err != nil {
+		panic(err)
+	}
+
+	if err := client.OpenGateway(context.Background()); err != nil {
+		panic(err)
+	}
+	defer client.Close(context.Background())
+
+	handleConsole(l, client)
+}
